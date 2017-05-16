fda9834d82c04af9b1b787c7838073b473cf9813
Wood <LWood@nisdtx.org>
2017-05-16 15:19:28 -0500

    add first _test.go

--- hello_altLocation/hello_alt.go
+++ hello_altLocation/hello_alt.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/lwood54/Udemy/stringutil"
+)
 
 func main() {
-	fmt.Println("Hello from an alternate location.")
+	fmt.Println(stringutil.Reverse("Hello from an alternate location."))
 }
