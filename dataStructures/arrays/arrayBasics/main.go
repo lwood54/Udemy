57776e45bf3fa4d0451e19cd08db3d8e4e8127f9
Logan Wood <lwood3499imac@logans-imac.attlocal.net>
2017-06-28 19:51:43 -0500

    intro to arrays in Go

---
+++ dataStructures/arrays/arrayBasics/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+// Arrays have to have a predetermined length, and that can't be changed
+// after they have been created.
+func main() {
+	var x [58]string
+	fmt.Println(x)
+	fmt.Println(len(x))
+	fmt.Println(x[42])
+	for i := 65; i <= 122; i++ {
+		x[i-65] = fmt.Sprintln(i, ": ", string(i))
+	}
+	fmt.Println(x)
+	fmt.Println(len(x))
+	fmt.Println(x[42])
+}
